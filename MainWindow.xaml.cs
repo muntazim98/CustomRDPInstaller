b9931b0a5cb909e384b950e05ac3b36ad61e911a
muntazimalikhan <muntazimalikhan@globussoft.in>
2025-10-06 11:37:27 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CustomRDPInstaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
