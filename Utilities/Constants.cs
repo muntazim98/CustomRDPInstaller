dc2b3935d470c5e22a53d357cee4496131367992
muntazimalikhan <muntazimalikhan@globussoft.in>
2025-10-08 17:56:42 +0530

    ++Updated Zip Link++

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -14,7 +14,7 @@ namespace AltraVeraHostInstaller.Utilities
         public static double UIOpacityEnable = 1;
         public static double UIOpacityDisable = 0.8;
         public static string ZipPath => $"{ApplicationName}.zip";
-        public static Uri uri { get; set; } = new Uri(@"https://www.dropbox.com/scl/fi/gse2k878b6jhwbq4bhqc3/AltraVeraHost.zip?rlkey=ean9hvs29jo2kbhp7oc4pnzgj&dl=1");
+        public static Uri uri { get; set; } = new Uri(@"https://www.dropbox.com/scl/fi/494s0xd25ddxvpdvfzc6o/AltraVeraHost.zip?rlkey=ph636jxpv1wvswk2vc2hz7r51&dl=1");
         public static string GetLocalFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string InstallerFolder => $"{GetLocalFolder}\\{ApplicationName}Installer";
         public static string AssemblyName => System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
