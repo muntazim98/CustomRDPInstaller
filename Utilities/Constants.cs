2d6facf0b2dcf503fa3e7a7e94e657091cd0f8ab
Rahmatullah_Ansari <rahmatullahansari@globussoft.in>
2025-10-08 11:42:53 +0530

    ++Updated Agent zip folder and name

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -11,16 +11,13 @@ namespace CustomRDPInstaller.Utilities
         public static string GetDefaultIntallationPathX86 { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
         public static string GetDefaultIntallationPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
         public static string ApplicationName { get; set; } = "AltraVera";
-
-       
-
         public static string ConfirmationMessageForClosing { get; set; } = $"{ServiceName} is running... Do you want to stop & uninstall ?";
         public static double UIOpacityEnable = 1;
         public static double UIOpacityDisable = 0.8;
         public static string ServiceName => "AltraVeraAgentService";
         public static string ServiceExeName => "AltraVera_agent_service.exe";
         public static string ZipPath => $"{ApplicationName}Service.zip";
-        public static Uri uri { get; set; } = new Uri(@"https://www.dropbox.com/scl/fi/4t9xzulxa3ltn5tgsj2z8/AltraVeraAgentService.zip?rlkey=ls6mjingtn3lllnmbi7w557m1&dl=1");
+        public static Uri uri { get; set; } = new Uri(@"https://www.dropbox.com/scl/fi/ga8w32fmbkhsp49djnbp4/AltraVeraAgent.zip?rlkey=r9k7dshcuhajfukafv9d0p3h4&dl=1");
         public static string GetLocalFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string InstallerFolder => $"{GetLocalFolder}\\{ApplicationName}Installer";
         public static string AssemblyName => System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
