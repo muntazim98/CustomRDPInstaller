7790cea7993b23305838642035790ed2847bc545
muntazimalikhan <muntazimalikhan@globussoft.in>
2025-10-09 19:49:01 +0530

    ++Updated URL

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -18,7 +18,7 @@ namespace CustomRDPInstaller.Utilities
         public static string ServiceName => "AltraVeraAgentService";
         public static string ServiceExeName => "AltraVera_agent_service.exe";
         public static string ZipPath => $"{ApplicationName}Service.zip";
-        public static Uri uri { get; set; } = new Uri(@"https://www.dropbox.com/scl/fi/he6wi0mere66czb7h958y/AltraVeraAgent.zip?rlkey=50m7ldtaxkp1k8qd3a5rwg8u9&st=czdxyza8&dl=1");
+        public static Uri uri { get; set; } = new Uri(@"https://www.dropbox.com/scl/fi/nrj1th2xz1w6so2zog9p3/AltraVeraAgent.zip?rlkey=zlpji3leybhjzznvmfnknkzgf&dl=1");
         public static string GetLocalFolder => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
         public static string InstallerFolder => $"{GetLocalFolder}\\{ApplicationName}Installer";
         public static string AssemblyName => System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
